5c3b9f3954b054fb0614c3a3766ef6361405c2fb
ShuXian <test27c@gmail.com>
2017-05-01 18:34:18 +0700

    style todo application

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -13,7 +13,7 @@ var AddTodo = React.createClass({
   },
   render: function () {
     return (
-      <div>
+      <div className="container__footer">
         <form ref="form" onSubmit={this.onSubmit} className="add-todo-form">
           <input type="text" ref="todoText" placeholder="What do you need to do?"/>
           <button className="button expanded">Add Todo</button>
